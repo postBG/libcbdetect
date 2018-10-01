2cf6480312884064f30dfe8caba458151eb1f90c
hyunmin-lee <hyunmin.lee@naverlabs.com>
2018-10-01 19:10:45 +0900

    vectorization

--- code/create_correlation_patch.py
+++ code/create_correlation_patch.py
@@ -29,6 +29,33 @@ def createCorrelationPatch(angle_1, angle_2, radius):
     n2 = [-np.sin(angle_2), np.cos(angle_2)]
 
     # for all points in template do
+    vecs = np.mgrid[0:patch_size, 0:patch_size].T.reshape(-1, 2)
+    vecs = vecs - [radius, radius]
+
+    s1s = np.matmul(vecs, np.transpose(n1))
+    s2s = np.matmul(vecs, np.transpose(n2))
+
+    dists = np.linalg.norm(vecs, axis=1)
+    pdfs = norm.pdf(dists, 0, radius / 2)
+
+    for v in range(0, patch_size):
+        for u in range(0, patch_size):
+            # check on which side of the normals we are
+            s1 = s1s[v * patch_size + u]
+            s2 = s2s[v * patch_size + u]
+
+            pdf = pdfs[v * patch_size + u]
+            if s1 <= -0.1 and s2 <= -0.1:
+                template.a1[v][u] = pdf
+            elif s1 >= 0.1 and s2 >= 0.1:
+                template.a2[v][u] = pdf
+            elif s1 <= -0.1 and s2 >= 0.1:
+                template.b1[v][u] = pdf
+            elif s1 >= 0.1 and s2 <= -0.1:
+                template.b2[v][u] = pdf
+
+    '''
+    # for all points in template do
     for v in range(0, patch_size):
         for u in range(0, patch_size):
             # vector
@@ -46,6 +73,8 @@ def createCorrelationPatch(angle_1, angle_2, radius):
                 template.b1[v][u] = norm.pdf(np.linalg.norm(vec), 0, radius/2)
             elif (s1 >= 0.1 and s2 <= -0.1):
                 template.b2[v][u] = norm.pdf(np.linalg.norm(vec), 0, radius/2)
+    '''
+
     # normalize
     template.do_normalize()
     return template
