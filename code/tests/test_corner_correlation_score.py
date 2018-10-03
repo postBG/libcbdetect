0075bfa4ef259ce8b8cfa1efb128b4172a87ae2a
postBG <profile2697@gmail.com>
2018-10-03 20:41:34 +0900

    add corner_correlation_score sample test

--- code/tests/test_corner_correlation_score.py
+++ code/tests/test_corner_correlation_score.py
@@ -1,51 +1,19 @@
 import unittest
-import numpy as np
+import pickle
 
-from create_correlation_patch import createCorrelationPatch
+from corner_correlation_score import cornerCorrelationScore
 
 
-class TestCreateCorrelationPathch(unittest.TestCase):
-    def test_createCorrelationPatch(self):
-        template = createCorrelationPatch(0., 0., 1)
+class CornerCorrelationScore(unittest.TestCase):
+    TEST_DATA_PATH = 'test_data/cornerCorrelationScore/test_input.pkl'
 
-        expected_a1 = np.array([[0.10650697891920068, 0.7869860421615987, 0.10650697891920068],
-                                [0.0, 0.0, 0.0],
-                                [0.0, 0.0, 0.0]])
-        expected_a2 = np.array(
-            [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.10650697891920068, 0.7869860421615987, 0.10650697891920068]])
-        expected_b1 = np.array([[np.nan, np.nan, np.nan], [np.nan, np.nan, np.nan], [np.nan, np.nan, np.nan]])
-        expected_b2 = np.array([[np.nan, np.nan, np.nan], [np.nan, np.nan, np.nan], [np.nan, np.nan, np.nan]])
+    def setUp(self):
+        with open(self.TEST_DATA_PATH, 'rb') as f:
+            test_data = pickle.load(f)
+            self.img_sub = test_data['img_sub']
+            self.img_weight_sub = test_data['img_weight_sub']
+            self.v1 = test_data['v1']
+            self.v2 = test_data['v2']
 
-        np.testing.assert_almost_equal(template.a1, expected_a1)
-        np.testing.assert_almost_equal(template.a2, expected_a2)
-        np.testing.assert_almost_equal(template.b1, expected_b1)
-        np.testing.assert_almost_equal(template.b2, expected_b2)
-
-    def test_createCorrelationPatch2(self):
-        template = createCorrelationPatch(0.4, 0.4, 2)
-        expected_a1 = np.array([[0.007086828361808099, 0.031760961212477186, 0.05236497232889289, 0.031760961212477186,
-                                 0.007086828361808099],
-                                [0.031760961212477186, 0.1423427527293902, 0.23468352415495441, 0.1423427527293902,
-                                 0.031760961212477186], [0.0, 0.0, 0.0, 0.23468352415495441, 0.05236497232889289],
-                                [0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0]])
-        expected_a2 = np.array(
-            [[0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0],
-             [0.05236497232889289, 0.23468352415495441, 0.0, 0.0, 0.0],
-             [0.031760961212477186, 0.1423427527293902, 0.23468352415495441, 0.1423427527293902, 0.031760961212477186],
-             [0.007086828361808099, 0.031760961212477186, 0.05236497232889289, 0.031760961212477186,
-              0.007086828361808099]])
-        expected_b1 = np.array([[np.nan, np.nan, np.nan, np.nan, np.nan],
-                                [np.nan, np.nan, np.nan, np.nan, np.nan],
-                                [np.nan, np.nan, np.nan, np.nan, np.nan],
-                                [np.nan, np.nan, np.nan, np.nan, np.nan],
-                                [np.nan, np.nan, np.nan, np.nan, np.nan]])
-        expected_b2 = np.array([[np.nan, np.nan, np.nan, np.nan, np.nan],
-                                [np.nan, np.nan, np.nan, np.nan, np.nan],
-                                [np.nan, np.nan, np.nan, np.nan, np.nan],
-                                [np.nan, np.nan, np.nan, np.nan, np.nan],
-                                [np.nan, np.nan, np.nan, np.nan, np.nan]])
-
-        np.testing.assert_almost_equal(template.a1, expected_a1)
-        np.testing.assert_almost_equal(template.a2, expected_a2)
-        np.testing.assert_almost_equal(template.b1, expected_b1)
-        np.testing.assert_almost_equal(template.b2, expected_b2)
+    def test_cornerCorrelationScore(self):
+        self.assertAlmostEqual(0.0, cornerCorrelationScore(self.img_sub, self.img_weight_sub, self.v1, self.v2))
